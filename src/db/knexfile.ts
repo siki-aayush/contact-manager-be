d507b0b823b335927f855a40bf078abc8692e1f2
siki <musyajuaayush@gmail.com>
2022-08-17 09:50:50 +0545

    modify: changes the knexfile configs

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -2,12 +2,28 @@ import dotenv from "dotenv";
 import type { Knex } from "knex";
 import path from "path";
 
-dotenv.config({
-  path: path.join(__dirname, "../../.env"),
-});
+// dotenv.config({
+//   path: path.join(__dirname, "../../.env"),
+// });
 
 export const config: { [key: string]: Knex.Config } = {
   development: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      // ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
     client: "pg",
     connection: {
       database: process.env.DB_NAME,
